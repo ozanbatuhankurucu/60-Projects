e0d4dac171542e066e942cc2e4dd5b919f9ec0fb
ozan batuhan kurucu <ozanbatuhankurucu@ozans-MacBook-Pro.local>
2021-03-28 21:38:59 +0300

    expanding pictures completed

--- projects/expanded_pictures/script.js
+++ projects/expanded_pictures/script.js
@@ -1,1 +1,19 @@
+const allPics = document.querySelectorAll('.pic')
+console.log(allPics)
 
+allPics.forEach((pic,index) => {
+  
+  pic.addEventListener('click', () => {
+    removeAllActiveClasses(index)
+    pic.classList.add('active')
+  })
+})
+
+function removeAllActiveClasses(index) {
+  allPics.forEach((pic,indexP) => {
+      if(indexP !== index){
+        pic.classList.remove('active')
+      }
+   
+  })
+}
